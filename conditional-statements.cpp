a94e13515ee084213bee7b21efb03abdbd2e4e23
Linda Lim <CookiesNCream@users.noreply.github.com>
2017-03-21 14:37:01 +0800

    Create conditional-statements.cpp

---
+++ conditional-statements.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  if(n == 1) {
+      cin >> n;
+      cout << "one\n";
+  }
+  else if(n == 2){
+      cin >> n;
+      cout << "two\n";
+  }
+  else if(n == 3){
+      cin >> n;
+      cout << "three\n";
+  }
+  else if(n == 4){
+      cin >> n;
+      cout << "four\n";
+  }
+  else if(n == 5){
+      cin >> n;
+      cout << "five\n";
+  }
+  else if(n == 6){
+      cin >> n;
+      cout << "six\n";
+  }
+  else if(n == 7){
+      cin >> n;
+      cout << "seven\n";
+  }
+  else if(n == 8){
+      cin >> n;
+      cout << "eight\n";
+  }
+  else if(n == 9){
+      cin >> n;
+      cout << "nine\n";
+  }
+  else {
+       cin >> n;
+       cout << "Greater than 9\n";
+   }
+   return 0;
+}
